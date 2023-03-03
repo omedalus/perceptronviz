5ff207c2acee424d69638594335220447d3af03d
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2023-03-03 11:32:00 -0500

    Got rid of some default junk, and made prettier how I like it.

---
+++ www/src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const elemSpacer = ref();
+const elemHeader = ref();
+
+const adjustSpacerSizeToMatchHeader = () => {};
+</script>
+
+<template>
+  <div class="header-magic-static-spacer" ref="elemSpacer"></div>
+  <header ref="elemHeader">
+    <div class="header-contents">
+      <div class="header-title">Perceptron visualization</div>
+      <div class="header-text">Learn the basics of how very simple neural networks work</div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header-magic-static-spacer {
+}
+
+header {
+  background-color: #444;
+  text-align: center;
+  height: 3em;
+
+  .header-contents {
+    max-width: 1024px;
+    margin: auto;
+  }
+}
+</style>
