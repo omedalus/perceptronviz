6d8ecbdfc67fe8a4ee83594e2da0f4031c3a4eb5
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2023-03-05 19:58:02 -0500

    Training area is coming along.

--- www/src/App.vue
+++ www/src/App.vue
@@ -46,8 +46,8 @@ updateViz();
 
         <div>
           <p>
-            <span style="font-weight: bold">Draw a 5x5-pixel picture on this grid.</span><br />
-            <span>Hold down on a pixel (square) to change its brightness level (its value).</span>
+            <span style="font-weight: bold">Draw a 5x5-pixel picture.</span><br />
+            <span>Hold down on a pixel (square) to change its brightness value.</span>
           </p>
         </div>
 
@@ -74,8 +74,37 @@ updateViz();
           </div>
         </div>
       </div>
-      <div class="interactive-panel interactive-panel--outputs">Outputs go here</div>
-      <div class="interactive-panel interactive-panel--network">Diagram of network goes here</div>
+      <div class="interactive-panel interactive-panel--training">
+        <h2>Training</h2>
+        <div class="training-fields">
+          <div class="training-field-enter-name">
+            <label for="currentimagelabel">Give your image a name: </label>
+            <input id="currentimagelabel" type="text" />
+          </div>
+          <div class="training-field-choose-name">
+            <label for="currentimagedropdown">Or choose a name you've already created: </label>
+            <select id="currentimagedropdown" />
+          </div>
+          <div class="training-delete-name">
+            <a @click="">Delete this name</a>
+          </div>
+        </div>
+        <div class="training-instructions">
+          By <strong>training</strong> the perceptron, you teach it to associate this image with
+          this name (or to dissociate them).
+        </div>
+        <div class="training-controls">
+          <div class="training-button training-button--associate">
+            <strong>Yes</strong>, this image is ______.
+          </div>
+          <div class="training-button training-button--dissociate">
+            <strong>No</strong>, this image is not ______.
+          </div>
+        </div>
+      </div>
+      <div class="interactive-panel interactive-panel--output">
+        <h2>Output</h2>
+      </div>
     </div>
   </main>
 
@@ -345,9 +374,6 @@ updateViz();
     flex: 1;
     margin: 1em;
     text-align: center;
-  }
-
-  .interactive-panel--grid {
     min-width: 16em;
   }
 
@@ -356,10 +382,10 @@ updateViz();
   }
 
   .gridmodel-clear-input {
-    width: 12em;
+    width: 15em;
     margin: auto;
     text-align: right;
-    font-size: 0.875rem;
+    font-size: 0.75rem;
     margin-bottom: 1em;
   }
 }
@@ -373,6 +399,61 @@ updateViz();
 
   .vector-readout-value {
     margin-left: 0.5em;
+  }
+}
+
+.training-fields {
+  text-align: right;
+
+  label {
+    font-size: 0.875rem;
+  }
+
+  input {
+    width: 12ex;
+  }
+  select {
+    width: 13ex;
+  }
+
+  .training-delete-name {
+    font-size: 0.75rem;
+    margin-top: 0.25ex;
+  }
+}
+.training-instructions {
+  margin-top: 0.5em;
+}
+.training-controls {
+  display: flex;
+  flex-direction: row;
+  gap: 1em;
+  margin-top: 1em;
+
+  .training-button {
+    flex: 1;
+    opacity: 0.8;
+    cursor: pointer;
+    border-radius: 1ex;
+    padding: 0.5em;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    &:active {
+      filter: saturate(0.5) brightness(1.25);
+    }
+
+    &.training-button--associate {
+      color: #8f8;
+      background-color: #8f86;
+    }
+
+    &.training-button--dissociate {
+      color: #f88;
+      background-color: #f886;
+    }
   }
 }
 </style>
