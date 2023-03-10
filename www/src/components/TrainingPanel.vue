7d13c65a5377fba79f42e33dbcddab19203c02f7
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2023-03-10 18:24:01 -0500

    Making the training overlay appear and disappear.

--- www/src/components/TrainingPanel.vue
+++ www/src/components/TrainingPanel.vue
@@ -13,7 +13,7 @@ const emit = defineEmits<{
 }>();
 
 const timerTrainingAnimation = ref(0);
-const MS_TRAINING_ANIMATION_DURATION = 1000;
+const MS_TRAINING_ANIMATION_DURATION = 2500;
 const isTrainingAnimationRunning = computed(() => {
   return !!timerTrainingAnimation.value;
 });
@@ -24,11 +24,20 @@ const train = (reinforcementFactor: number) => {
     return;
   }
 
+  if (reinforcementFactor > 0) {
+    trainingHeatgridOverlayDirection.value = 'up';
+  } else {
+    trainingHeatgridOverlayDirection.value = 'down';
+  }
+
   timerTrainingAnimation.value = window.setTimeout(() => {
     timerTrainingAnimation.value = 0;
     props.modelValue.trainCurrentOutput(reinforcementFactor);
+    trainingHeatgridOverlayDirection.value = 'none';
   }, MS_TRAINING_ANIMATION_DURATION);
 };
+
+const trainingHeatgridOverlayDirection = ref('none');
 
 onBeforeUnmount(() => {
   window.clearTimeout(timerTrainingAnimation.value);
@@ -51,9 +60,9 @@ onBeforeUnmount(() => {
         set to random values.
       </p>
       <p>
-        By <strong>training</strong> the perceptron, you strengthen connections that lead to the
-        correct output, and weaken connections that lead to incorrect ones. As a result, the
-        perceptron "learns" to assign the correct label to the image.
+        By <strong>training</strong> the perceptron, you strengthen active connections that lead to
+        the correct output, and weaken active connections that lead to incorrect ones. As a result,
+        the perceptron "learns" to assign the correct label to the image.
       </p>
     </div>
 
@@ -65,36 +74,74 @@ onBeforeUnmount(() => {
             :dim="modelValue.dim"
             :darkfloor="0"
           ></HeatGrid>
-          <div class="training-heatgrid-overlay">
+          <div
+            class="training-heatgrid-overlay"
+            :class="{ 'overlay-showing': trainingHeatgridOverlayDirection !== 'none' }"
+          >
             <HeatGrid :vector="modelValue.input" :dim="modelValue.dim" input-overlay></HeatGrid>
+            <div class="training-heatgrid-overlay-actual training-heatgrid-overlay-add"></div>
           </div>
         </div>
       </div>
 
       <div class="training-current-assessment">
-        <HeatGrid :vector="modelValue.input" :dim="modelValue.dim" input-overlay></HeatGrid>
+        <div
+          class="training-controls"
+          :class="{
+            'training-animation-is-running': isTrainingAnimationRunning,
+            'training-animation-is-not-running': !isTrainingAnimationRunning
+          }"
+        >
+          <div style="flex-grow: 0; margin-right: 1em">
+            <HeatGrid
+              :vector="modelValue.input"
+              :dim="modelValue.dim"
+              input-overlay
+              skip-bias
+            ></HeatGrid>
+          </div>
+          <div style="text-align: left">
+            <div v-if="modelValue.assess()">
+              <div style="color: green">
+                The perceptron currently "thinks" that this image depicts
+                <strong>{{ modelValue.currentOutputLabel }}</strong>
+              </div>
 
-        <div v-if="modelValue.assess()">The perceptron currently thinks that</div>
-      </div>
+              <div class="explanation-text">
+                If it's right, leave it alone. If it's wrong, <strong>apply training</strong>&nbsp;
+                <span style="color: #888"
+                  >(it may take several trainings for the perceptron to change its "mind")</span
+                >:
+              </div>
 
-      <div
-        class="training-controls"
-        :class="{
-          'training-animation-is-running': isTrainingAnimationRunning,
-          'training-animation-is-not-running': !isTrainingAnimationRunning
-        }"
-      >
-        <div class="training-button training-button--associate" @click="train(1)">
-          <img src="@/assets/img/green-check.png" />
-          <strong>Yes</strong>, this image is
-          <span class="outputlabel">{{ modelValue.currentOutputLabel }}</span
-          >.
-        </div>
-        <div class="training-button training-button--dissociate" @click="train(-1)">
-          <img src="@/assets/img/red-x.png" />
-          <strong>No</strong>, this image is not
-          <span class="outputlabel">{{ modelValue.currentOutputLabel }}</span
-          >.
+              <div class="training-button training-button--dissociate" @click="train(-1)">
+                <img src="@/assets/img/red-x.png" />
+                <strong>No</strong>, this image is not
+                <span class="outputlabel">{{ modelValue.currentOutputLabel }}</span
+                >.
+              </div>
+            </div>
+            <div v-else>
+              <div style="color: red">
+                The perceptron currently "thinks" that this image <em>does not</em> depict
+                <strong>{{ modelValue.currentOutputLabel }}</strong>
+              </div>
+
+              <div class="explanation-text">
+                If it's right, leave it alone. If it's wrong, <strong>apply training</strong>&nbsp;
+                <span style="color: #888"
+                  >(it may take several trainings for the perceptron to change its "mind")</span
+                >:
+              </div>
+
+              <div class="training-button training-button--associate" @click="train(1)">
+                <img src="@/assets/img/green-check.png" />
+                <strong>Yes</strong>, this image is
+                <span class="outputlabel">{{ modelValue.currentOutputLabel }}</span
+                >.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -102,6 +149,20 @@ onBeforeUnmount(() => {
 </template>
 
 <style scoped lang="scss">
+@keyframes heatgrid-overlay {
+  0% {
+    opacity: 0;
+    transform: scale(1.25);
+  }
+  20% {
+    opacity: 0.75;
+  }
+  100% {
+    opacity: 0;
+    transform: scale(1);
+  }
+}
+
 .training-panel {
   .training-instructions {
     margin-top: 0.5em;
@@ -113,7 +174,6 @@ onBeforeUnmount(() => {
     flex-direction: row;
     gap: 1em;
     margin-top: 0.5em;
-    position: relative;
 
     &.training-animation-is-running {
       filter: saturate(0.25);
@@ -137,6 +197,7 @@ onBeforeUnmount(() => {
       padding: 0.5em;
       font-size: 0.875em;
       user-select: none;
+      margin-top: 1ex;
 
       transition: opacity 0.25s, filter 0.25s;
 
@@ -182,14 +243,13 @@ onBeforeUnmount(() => {
 
   .training-heatgrid {
     margin-top: 1em;
-    margin-bottom: 1em;
+    margin-bottom: 0.5em;
 
     .training-heatgrid-positioner {
       display: inline-block;
       position: relative;
 
       .training-heatgrid-overlay {
-        border: 1px solid red;
         display: inline-block;
         position: absolute;
         top: 0;
@@ -198,7 +258,12 @@ onBeforeUnmount(() => {
         height: 100%;
 
         z-index: 10;
-        transform: scale(1.2);
+        display: none;
+
+        &.overlay-showing {
+          display: block;
+          animation: heatgrid-overlay 2500ms;
+        }
       }
     }
   }
